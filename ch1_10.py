fda99e4fccd438724e74c3a9f3b40736ba3afc93
highsociety2 <amcknight@ideaventionsacademy.org>
2017-11-13 12:03:52 -0500

    Problem 11

--- ch1_10.py
+++ ch1_10.py
@@ -4,12 +4,16 @@
 
 man = 1
 Man = int(man)
+
 wife = Man * 7
 Wife = int(wife)
+
 sack = Wife * 7
 Sack = int(sack)
+
 cat = Sack * 7
 Cat = int(cat)
+
 kitten = Cat * 7
 Kitten = int(kitten)
 
---
+++ ch1_11.py
@@ -0,0 +1,7 @@
+# Name
+# Problem 11
+# Chapter 1 Problem Set Part 1
+
+my_int = 5
+my_int += my_int + 3
+print(my_int)
