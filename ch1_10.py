a9a47486f68609a092bf7d22c22077acf59dd133
highsociety2 <amcknight@ideaventionsacademy.org>
2017-11-13 11:48:26 -0500

    Problem 10

--- ch1_10.py
+++ ch1_10.py
@@ -1,3 +1,20 @@
-# Name
-# Problem #
+# Nathan Rabinovitch
+# Problem 10
 # Chapter 1 Problem Set Part 1
+
+man = 1
+Man = int(man)
+wife = Man * 7
+Wife = int(wife)
+sack = Wife * 7
+Sack = int(sack)
+cat = Sack * 7
+Cat = int(cat)
+kitten = Cat * 7
+Kitten = int(kitten)
+
+amount = Man + Wife + Sack + Cat + Kitten 
+
+Amount = int(amount)
+
+print(amount)
