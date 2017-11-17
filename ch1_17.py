477a0e9d6ddaf5cb65b9c8ec61704153ed002b55
highsociety2 <amcknight@ideaventionsacademy.org>
2017-11-17 15:17:13 -0500

    ghkjh

--- ch1_17.py
+++ ch1_17.py
@@ -1,3 +1,14 @@
-# Name
-# Problem #
+# Nathan R
+# Problem 17
 # Chapter 1 Problem Set Part 1
+
+
+something = input("Insert an input here:  ")
+print(something)
+
+#Something = int(something)
+#print(Something)
+
+something_float = float(something)
+print(something_float)
+
