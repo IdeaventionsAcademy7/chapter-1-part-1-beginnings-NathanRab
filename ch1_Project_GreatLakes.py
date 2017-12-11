f54edfbb7cd27810be2c26c2a96c2ef2e3d299e4
rheitz <rheitz@ideaventionsacademy.org>
2017-12-11 10:46:38 -0500

    Update ch1_Project_GreatLakes.py

--- ch1_Project_GreatLakes.py
+++ ch1_Project_GreatLakes.py
@@ -2,7 +2,7 @@
 # Project: Great Lakes are how big?
 # Chapter 1 Problem Set Part 1
 
-
+# RCH: Please use lowercase variable names and underscores between words
 KmsquaredofusA = 8080464.3
 GreatLakesV = 22810
 
